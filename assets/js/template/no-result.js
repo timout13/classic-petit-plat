49d3b7fd60201a2efce800df9a8077328de31025
Timothe Lebrun <timothe463@icloud.com>
2024-01-28 16:18:27 +0100

    refractor: rm log

--- assets/js/template/no-result.js
+++ assets/js/template/no-result.js
@@ -1,6 +1,5 @@
 export function noResultText(userSearch) {
     const template_noresult = document.querySelector("#template_noresult");
-    console.log(template_noresult);
     let node = template_noresult.content.cloneNode(true);
     const no_result_text = node.querySelector(".no_result_text");
     no_result_text.textContent = `Aucune recette ne contient ‘${userSearch}’ vous pouvez chercher « tarte aux pommes », « poisson », etc.`;
